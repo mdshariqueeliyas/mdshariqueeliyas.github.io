eda704244b89295626424925cd9d6226a9ad0907
shaeli1996 <md.sharique.eliyas@gmail.com>
2017-10-08 10:11:45 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 function open_modal(id){
 	var e = document.getElementById('modal');
@@ -30,68 +28,4 @@ function CopyToClipboard(id){
 		window.getSelection().addRange(range);
 		document.execCommand("Copy");
 	}
-=======
-
-function open_modal(id){
-	var e = document.getElementById('modal');
-	var f = document.getElementById('modalinside');
-	if(e.style.visibility=="hidden" || e.style.visibility==""){
-		e.style.visibility = "visible";
-		f.className += " animated slideInDown";
-	}
-	else{
-		e.style.visibility = "hidden";
-		f.className = "";
-	}
-}
-
-function CopyToClipboard(id){
-	var f = document.getElementById('modalinside');
-	f.className="";
-	f.className += " animated shake";
-	if(document.selection){
-		var range = document.body.createTextRange();
-		range.moveToElementText(document.getElementById(id));
-		range.select().createTextRange();
-		document.execCommand("Copy");
-	}
-	else if(window.getSelection){
-		var range = document.createRange();
-		range.selectNode(document.getElementById(id));
-		window.getSelection().addRange(range);
-		document.execCommand("Copy");
-	}
->>>>>>> 4e60156fbaa969140670f38f42c2f81ee748500f
-=======
-
-function open_modal(id){
-	var e = document.getElementById('modal');
-	var f = document.getElementById('modalinside');
-	if(e.style.visibility=="hidden" || e.style.visibility==""){
-		e.style.visibility = "visible";
-		f.className += " animated slideInDown";
-	}
-	else{
-		e.style.visibility = "hidden";
-		f.className = "";
-	}
-}
-
-function CopyToClipboard(id){
-	var f = document.getElementById('modalinside');
-	f.className="";
-	f.className += " animated shake";
-	if(document.selection){
-		var range = document.body.createTextRange();
-		range.moveToElementText(document.getElementById(id));
-		range.select().createTextRange();
-		document.execCommand("Copy");
-	}
-	else if(window.getSelection){
-		var range = document.createRange();
-		range.selectNode(document.getElementById(id));
-		window.getSelection().addRange(range);
-		document.execCommand("Copy");
-	}
->>>>>>> 4e60156fbaa969140670f38f42c2f81ee748500f
 }
